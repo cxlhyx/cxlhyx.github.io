19b8c960413b33fd69589a2565f0d001340dce2e
“cxlhyx” <1297767084@QQ.com>
2024-05-29 14:28:14 +0800

    Update resume

---
+++ resume/assets/js/index.js
@@ -0,0 +1,4 @@
+console.log("感谢您的浏览，期待能够加入贵公司！")
+console.log("我的邮箱：cxlhyx1297767084@gmail.com");
+console.log("我的GitHub：https://github.com/cxlhyx");
+console.log("我的CSDN博客：https://blog.csdn.net/muaamua");
